d1fcd4bdbfccc5dfa80c56729c3ed30494f6b01c
Greg Bergé <berge.greg@gmail.com>
2022-10-02 14:25:28 +0200

    chore: fix font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato:medium,bold,regular&display=optional"
+            href="https://fonts.googleapis.com/css?family=Lato:400,500,700&display=swap"
             rel="stylesheet"
           />
         </Head>
