e91f3153af0cd03634d25985cf8f028e1173ba2e
Greg Bergé <berge.greg@gmail.com>
2023-11-02 13:24:30 +0100

    chore: minor fixes for Safari

--- app/home/hero/Hero.tsx
+++ app/home/hero/Hero.tsx
@@ -38,7 +38,7 @@ export function Hero() {
         <div className="flex flex-col items-center gap-x-6 gap-y-12 py-8 md:py-24 lg:flex-row">
           <div className="flex flex-col items-center gap-6 text-center lg:items-start lg:text-left">
             <h1 className="bg-hero-text-gradient bg-clip-text font-accent text-[3.2rem] leading-tight text-transparent md:text-7xl lg:bg-hero-text-gradient-lg lg:leading-none">
-              Visual Testing <br className="hidden lg:block" />
+              Visual Testing <br />
               for developers
             </h1>
             <div className="text-xl text-low">
@@ -64,13 +64,13 @@ export function Hero() {
           <div className="relative mt-4 flex flex-1 items-center justify-end md:mt-0">
             <div className="relative z-10 w-[80vw] lg:w-[85%]">
               <a
-                className="group relative"
+                className="group relative block"
                 target="_blank"
                 href="https://app.argos-ci.com/argos-ci/argos-ci.com/builds/20/31313699"
               >
                 <div className="absolute inset-0 overflow-hidden rounded bg-gradient-to-br from-violetA-11 to-plumA-10 opacity-0 transition duration-500 group-hover:opacity-100" />
                 <div className="absolute inset-0 flex scale-50 transform items-center justify-center rounded opacity-0 transition duration-500 group-hover:scale-100 group-hover:opacity-100">
-                  <div className="font-accent text-4xl text-mauve-1 drop-shadow-lg">
+                  <div className="font-accent text-2xl text-mauve-1 drop-shadow-lg md:text-4xl">
                     See a demo build
                   </div>
                 </div>
