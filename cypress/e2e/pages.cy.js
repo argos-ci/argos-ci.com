2cd16888a3ebdc8f52fc29386cb89a8ed41701a5
Jeremy Sfez <jeremy@smooth-code.com>
2023-12-05 15:39:11 +0100

    feat: upgrade argos-cypress

--- cypress/e2e/pages.cy.js
+++ cypress/e2e/pages.cy.js
@@ -1,60 +1,22 @@
-const pages = {
-  homepage: "/",
-  terms: "/terms",
-  privacy: "/privacy",
-  security: "/security",
-  blog: "/blog",
-  pricing: "/pricing",
-  "blog-post-1": "/blog/visual-testing",
-  "blog-post-2": "/blog/improve-dx",
-  "blog-post-3": "/blog/playwright",
-};
+const pages = [
+  { name: "homepage", path: "/" },
+  { name: "terms", path: "/terms" },
+  { name: "privacy", path: "/privacy" },
+  { name: "security", path: "/security" },
+  { name: "blog", path: "/blog" },
+  { name: "pricing", path: "/pricing" },
+  { name: "blog-post-1", path: "/blog/visual-testing" },
+  { name: "blog-post-2", path: "/blog/improve-dx" },
+  { name: "blog-post-3", path: "/blog/playwright" },
+];
 
-const viewportPresets = ["macbook-16", "ipad-2", "iphone-8"];
+const viewports = ["macbook-16", "ipad-2", "iphone-8"];
 
-function injectStyles(document, styles) {
-  const css = document.createElement("style");
-  css.type = "text/css";
-  css.textContent = styles;
-  document.body.appendChild(css);
-}
-
-viewportPresets.forEach((viewportPreset) => {
-  describe(viewportPreset, () => {
-    beforeEach(() => {
-      cy.viewport(viewportPreset);
+describe("Screenshot pages", () => {
+  for (const { name, path } of pages) {
+    it(`Screenshots for ${name}`, () => {
+      cy.visit(path);
+      cy.argosScreenshot(name, { viewports });
     });
-    Object.keys(pages).forEach((pageName) => {
-      const name = `${viewportPreset}/${pageName}`;
-
-      it(pageName, () => {
-        cy.visit(pages[pageName]);
-        cy.wait(200);
-        cy.document().then((document) => {
-          injectStyles(
-            document,
-            `
-            /* No sticky header */
-            nav {
-              position: initial !important;
-            }
-          `,
-          );
-        });
-        cy.waitUntil(() =>
-          cy.document().then((document) => {
-            const allImages = Array.from(document.images);
-            allImages.forEach((img) => {
-              img.loading = "eager";
-              img.decoding = "sync";
-            });
-            return allImages.every(
-              (img) => img.complete && img.naturalWidth > 0,
-            );
-          }),
-        );
-        cy.argosScreenshot(name);
-      });
-    });
-  });
+  }
 });
