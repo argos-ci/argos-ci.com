b0e9cd0aadbaaa55739827f9f1ea9fe53456d47b
Greg Bergé <berge.greg@gmail.com>
2023-12-27 23:50:03 +0400

    chore: upgrade cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,15 @@
 const { defineConfig } = require("cypress");
+const { registerArgosTask } = require("@argos-ci/cypress/task");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000/",
     video: false,
+    setupNodeEvents(on, config) {
+      registerArgosTask(on, config, {
+        // Enable upload to Argos only when it runs on CI.
+        uploadToArgos: !!process.env.CI,
+      });
+    },
   },
 });
