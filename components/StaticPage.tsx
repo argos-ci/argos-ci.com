d8b627637450ec0e38936bad98628ecc9d2ff131
Greg Bergé <berge.greg@gmail.com>
2024-01-26 14:01:07 +0100

    fix(static): fix dark mode on static pages

--- components/StaticPage.tsx
+++ components/StaticPage.tsx
@@ -3,7 +3,7 @@ import { Container } from "@/components/Container";
 export const StaticPage = ({ children }: { children: React.ReactNode }) => {
   return (
     <article
-      className="prose mx-auto mb-24 mt-14 max-w-none"
+      className="prose mx-auto mb-24 mt-14 max-w-none dark:prose-invert"
       style={{ contain: "none" }}
     >
       <Container>{children}</Container>
