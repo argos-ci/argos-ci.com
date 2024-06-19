ac9b6a34d58718276de5e476e92d631a96b8da4b
Greg Bergé <berge.greg@gmail.com>
2024-06-19 22:47:07 +0200

    feat: allow to access homepage without redirect

--- components/RedirectIfCookie.tsx
+++ components/RedirectIfCookie.tsx
@@ -4,7 +4,10 @@ import { useEffect } from "react";
 
 export function RedirectIfCookie() {
   useEffect(() => {
-    if (document.cookie.includes("argos_jwt")) {
+    if (
+      document.cookie.includes("argos_jwt") &&
+      window.location.pathname === "/"
+    ) {
       window.location.href = "https://app.argos-ci.com/";
     }
   }, []);
