92b571fe917e4c157a43814f5bddb8862b16550e
Greg Bergé <berge.greg@gmail.com>
2024-10-22 21:56:56 +0200

    fix: remove non-existing tweet

--- app/home/users/Users.tsx
+++ app/home/users/Users.tsx
@@ -10,9 +10,8 @@ export function Users() {
         <H2 className="mb-4 bg-gradient-to-r from-violet-11 to-crimson-11 bg-clip-text text-center text-transparent">
           Loved by more than 3K users
         </H2>
-        <div className="space-y-6 py-8 sm:columns-2 sm:gap-6 lg:columns-3">
+        <div className="space-y-6 py-8 sm:columns-2 sm:gap-6">
           <Tweet id="1724422233840287766" />
-          <Tweet id="1724408703204671691" />
           <Tweet id="1724833402866778214" />
         </div>
       </section>
