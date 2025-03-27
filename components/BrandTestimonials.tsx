697672da0bbe2aa2f4b2b72637c6dd0b0abf6537
Jeremy Sfez <jeremy@smooth-code.com>
2025-03-27 17:41:58 +0100

    feat: update customer list

--- components/BrandTestimonials.tsx
+++ components/BrandTestimonials.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import businessInsider from "@/images/brands/business-insider.svg";
 import cozy from "@/images/brands/cozy.svg";
+import defacto from "@/images/brands/defacto.svg";
 import doctolib from "@/images/brands/doctolib.svg";
 import forethought from "@/images/brands/forethought.svg";
 import gitbook from "@/images/brands/gitbook.svg";
@@ -14,9 +15,12 @@ import mui from "@/images/brands/mui.svg";
 import pivot from "@/images/brands/pivot.svg";
 import planable from "@/images/brands/planable.svg";
 import rapidata from "@/images/brands/rapidata.svg";
+import redVentures from "@/images/brands/red-ventures.svg";
 import rho from "@/images/brands/rho.svg";
 import sindri from "@/images/brands/sindri.svg";
 import sivo from "@/images/brands/sivo.svg";
+import theEconomist from "@/images/brands/the-economist.svg";
+import wiz from "@/images/brands/wiz.svg";
 import yotpo from "@/images/brands/yotpo.svg";
 
 import { Container } from "./Container";
@@ -26,6 +30,11 @@ export const BRANDS = {
   mui: { src: mui, alt: "MUI", url: "https://mui.com" },
   gitbook: { src: gitbook, alt: "GitBook", url: "https://www.gitbook.com" },
   yotpo: { src: yotpo, alt: "Yotpo", url: "https://www.yotpo.com" },
+  wiz: {
+    src: wiz,
+    alt: "Wiz",
+    url: "https://www.wiz.io",
+  },
   cozy: { src: cozy, alt: "Cozy", url: "https://cozy.io" },
   handsontable: {
     src: handsontable,
@@ -51,27 +60,46 @@ export const BRANDS = {
     alt: "Business Insider",
     url: "https://www.businessinsider.com",
   },
+  theEconomist: {
+    src: theEconomist,
+    alt: "The Economist",
+    url: "https://www.economist.com",
+  },
+  defacto: {
+    src: defacto,
+    alt: "defacto",
+    url: "https://www.getdefacto.com",
+  },
   forethought: {
     src: forethought,
     alt: "Forethought",
     url: "https://forethought.ai",
   },
+  redVentures: {
+    src: redVentures,
+    alt: "Red Ventures",
+    url: "https://www.redventures.com/",
+  },
 };
 
 const brands = [
   BRANDS.meta,
+  BRANDS.wiz,
   BRANDS.mui,
   BRANDS.gitbook,
   BRANDS.yotpo,
-  BRANDS.cozy,
-  BRANDS.handsontable,
   BRANDS.interactiveThings,
   BRANDS.lemonde,
-  BRANDS.doctolib,
-  BRANDS.planable,
-  BRANDS.rapidata,
   BRANDS.rho,
-  BRANDS.sivo,
+  BRANDS.businessInsider,
+  BRANDS.theEconomist,
+  BRANDS.cozy,
+  BRANDS.doctolib,
+  BRANDS.redVentures,
+  BRANDS.handsontable,
+  BRANDS.forethought,
+  BRANDS.mermaid,
+  BRANDS.defacto,
   BRANDS.pivot,
 ];
 
