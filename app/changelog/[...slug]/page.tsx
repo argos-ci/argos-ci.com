de6c14f785e80ed149a384dcec13812ee4c911d8
Greg Bergé <berge.greg@gmail.com>
2026-01-17 14:29:42 +0100

    fix(seo): improve titles

--- app/changelog/[...slug]/page.tsx
+++ app/changelog/[...slug]/page.tsx
@@ -34,7 +34,7 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   }
 
   const url = `https://argos-ci.com/changelog/${changelog.slug}`;
-  const title = `${changelog.title} · Changelog · Argos`;
+  const title = `${changelog.title} · Changelog`;
   const images = [changelog.image];
   return {
     title: {
