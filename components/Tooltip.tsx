ba325a4edd399693c4b28f8d64fed6b3f6810422
Jeremy Sfez <jeremy@smooth-code.com>
2026-04-22 15:29:18 +0200

    fix: hydratation error

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -11,7 +11,7 @@ import * as React from "react";
 
 export type TooltipProps = {
   content: React.ReactNode;
-  children: React.ReactNode;
+  children: React.ReactElement;
   delayDuration?: TooltipTrigger.Props["delay"];
 } & Omit<TooltipRoot.Props, "children"> &
   Pick<TooltipPositioner.Props, "side" | "sideOffset">;
@@ -32,12 +32,7 @@ export function Tooltip(props: TooltipProps) {
 
   return (
     <TooltipPrimitive.Root disableHoverablePopup {...rest}>
-      <TooltipPrimitive.Trigger
-        delay={delayDuration}
-        render={
-          React.isValidElement(children) ? children : <span>{children}</span>
-        }
-      />
+      <TooltipPrimitive.Trigger delay={delayDuration} render={children} />
       <TooltipPrimitive.Portal>
         <TooltipPrimitive.Positioner side={side} sideOffset={sideOffset}>
           <TooltipPrimitive.Popup className="bg-app text-default animate-in fade-in-0 zoom-in-95 data-[ending-style]:animate-out data-[ending-style]:fade-out-0 data-[ending-style]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 pointer-events-none z-50 overflow-hidden rounded-md border px-2 py-1 text-sm shadow-md">
